c31608fb181e3f2b71f88ab9686153dc5e3c0178
Heinrich Schiller <heinrich@heinrich-schiller.de>
2019-11-05 22:19:54 +0100

    #IMPLEMENT 'include mustache'

--- bootstrap.php
+++ bootstrap.php
@@ -30,6 +30,7 @@ error_reporting(-1);
 ini_set('display_errors', '1');
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/mustache/mustache/src/Mustache/Autoloader.php';
 
 use ProjectPlanner\Libraries\Application;
 
