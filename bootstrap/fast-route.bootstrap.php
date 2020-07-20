e0bd1ee859b2b9583b50e8c82947bd6ac19ab97d
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-07-20 10:44:20 +0200

    #IMPLEMENT 'nikic/fast-route code'

---
+++ bootstrap/fast-route.bootstrap.php
@@ -0,0 +1,33 @@
+<?php
+
+use FastRoute\Dispatcher;
+
+require ROOT_DIR . 'src/routes/web.php';
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        list($class, $method) = explode("/", $handler, 2);
+        call_user_func_array(array(new $class, $method), $vars);
+        break;
+}
