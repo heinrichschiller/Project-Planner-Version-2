8479b9812fe121cd1b3e1099fd04ca299c1f75b0
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-08-18 12:05:19 +0200

    #IMPLEMENT 'js code'

---
+++ public/assets/js/modules/contact/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let UIController = (() => {
+    let DOMstrings = {
+        clickableRow: '.clickable__row',
+    }
+
+    return {
+        getInput: () => {
+            return {
+                // some code here
+            }
+        },
+
+        getDOMstrings: () => {
+            return DOMstrings;
+        }
+    }
+})();
+
+let BaseController = ((UICtrl) => {
+
+    let DOM = UICtrl.getDOMstrings();
+
+    let setupEventListeners = () => {
+        document.querySelectorAll(DOM.clickableRow).forEach(item => {
+            item.addEventListener('click', () => {
+                window.location = item.dataset.href;
+            })
+        });        
+    }
+
+    return {
+        init: () => {
+            console.log('Contact-Index-Script has started');
+            setupEventListeners();
+        }
+    }
+})(UIController);
+
+BaseController.init();
