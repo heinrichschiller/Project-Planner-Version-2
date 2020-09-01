dfa3bd2dadda34e42ea54fbbed3726682a4d440c
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-09-01 10:11:32 +0200

    #IMPLEMENT 'table navigation and colors'

--- public/assets/js/modules/project/read.js
+++ public/assets/js/modules/project/read.js
@@ -2,7 +2,10 @@
 
 let UIController = (() => {
     let DOMstrings = {
-        editBtn: 'edit__btn'
+        clickableRow: '.clickable__row',
+        editBtn: 'edit__btn',
+        statusColor: '.status__color',
+        priorityColor: '.priority__color'
     }
 
     return {
@@ -18,6 +21,12 @@ let Controller = ((UICtrl) => {
 
     let setupEventListeners = () => {
 
+        document.querySelectorAll(DOM.clickableRow).forEach(item => {
+            item.addEventListener('click', () => {
+                window.location = item.dataset.href;
+            })
+        });        
+
         document.getElementById('edit__btn').addEventListener('click', () => {
             let editBtn = document.getElementById('edit__btn');
 
@@ -25,12 +34,40 @@ let Controller = ((UICtrl) => {
                 window.location = editBtn.dataset.location;
             }
         });
+
     }
 
+    let setupStatusColors = () => {
+        document.querySelectorAll(DOM.statusColor).forEach(item => {
+            switch( parseInt(item.dataset.status_id) ) {
+                case 1: item.classList.add('active'); break;
+                case 2: item.classList.add('inprocessing'); break;
+                case 3: item.classList.add('waiting'); break;
+                case 4: item.classList.add('done'); break;
+                case 5: item.classList.add('suspend'); break;
+                case 6: item.classList.add('in-planning'); break;
+            }
+        });
+    }
+
+    let setupPriorityColors = () => {
+        document.querySelectorAll(DOM.priorityColor).forEach(item => {
+            switch( parseInt(item.dataset.priority_id) ) {
+                case 1: item.classList.add('very-high'); break;
+                case 2: item.classList.add('high'); break;
+                case 3: item.classList.add('normal'); break;
+                case 4: item.classList.add('low'); break;
+            }
+        });
+    }
+    
     return {
         init: () => {
             console.log('Project-Read-Script has started');
+
             setupEventListeners();
+            setupStatusColors();
+            setupPriorityColors();
         }
     }
 })(UIController);
