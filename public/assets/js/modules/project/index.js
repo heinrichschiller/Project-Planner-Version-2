79129f38d042e353cf6c63223631267905e1f5bb
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-09-05 17:14:41 +0200

    #IMPLEMENT 'colors on status and priority columns'

--- public/assets/js/modules/project/index.js
+++ public/assets/js/modules/project/index.js
@@ -3,6 +3,8 @@
 let UIController = (() => {
     let DOMstrings = {
         clickableRow: '.clickable__row',
+        statusColor: '.status__color',
+        priorityColor: '.priority__color'
     }
 
     return {
@@ -30,10 +32,36 @@ let BaseController = ((UICtrl) => {
         });        
     }
 
+    let setupStatusColors = () => {
+        document.querySelectorAll(DOM.statusColor).forEach(item => {
+            switch( parseInt(item.dataset.status_id) ) {
+                case 1: item.classList.add('active'); break;
+                case 2: item.classList.add('inprocessing'); break;
+                case 3: item.classList.add('waiting'); break;
+                case 4: item.classList.add('done'); break;
+                case 5: item.classList.add('suspend'); break;
+                case 6: item.classList.add('in-planning'); break;
+            }
+        });
+    }
+
+    let setupPriorityColors = () => {
+        document.querySelectorAll(DOM.priorityColor).forEach(item => {
+            switch( parseInt(item.dataset.priority_id) ) {
+                case 1: item.classList.add('very-high'); break;
+                case 2: item.classList.add('high'); break;
+                case 3: item.classList.add('normal'); break;
+                case 4: item.classList.add('low'); break;
+            }
+        });
+    }
+
     return {
         init: () => {
             console.log('Project-Index-Script has started');
             setupEventListeners();
+            setupStatusColors();
+            setupPriorityColors();
         }
     }
 })(UIController);
