36ee1475c78fd4973696f48e3d9ddb9618f17473
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-09-28 15:55:07 +0200

    #CHANGE 'set return type'

--- src/Application/Actions/Dashboard/DashboardAction.php
+++ src/Application/Actions/Dashboard/DashboardAction.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 class DashboardAction extends Action
 {
-    public function __invoke(Request $request, Response $response, $args = [])
+    public function __invoke(Request $request, Response $response, $args = []): Response
     {
         return $this->render($response, 'dashboard/index');
     }
