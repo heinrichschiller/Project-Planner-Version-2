ff135632890c12ed5e96b7d66a8aac73f65f038f
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-09-28 15:21:52 +0200

    #IMPLEMENT 'abstract action class'

---
+++ src/Application/Actions/Action.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Application\Actions;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+abstract class Action
+{
+    protected $ci;
+
+    public function __construct(ContainerInterface $ci)
+    {
+        $this->ci = $ci;
+    }
+
+    protected function render(Response $response, string $template, $data = null)
+    {
+        $html = $this->ci->get('view')->render($template, $data);
+        $response->getBody()->write($html);
+
+        return $response;
+    }
+
+    abstract public function __invoke(Request $request, Response $response, $args = []);
+}
