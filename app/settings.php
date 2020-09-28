767519f3a71c7cc69013e66af1fc6affff9e168b
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-09-28 17:13:36 +0200

    #CHANGE 'added commentaries for doctrine options'

--- app/settings.php
+++ app/settings.php
@@ -35,17 +35,146 @@ return function (ContainerBuilder $containerBuilder)
             'displayErrorDetails' => true,
             
             'doctrine' => [
+
+                /*
+                |----------------------------------------------------------------------------
+                | Developer mode
+                |----------------------------------------------------------------------------
+                |
+                | - If `isDevMode` is true caching is done in memory with the ArrayCache.
+                |   Proxy objects are recreated on every request.
+                | - If `isDevMode` is false, check for Caches in the order APC, Xcache, 
+                |   Memcache (127.0.0.1:11211), Redis (127.0.0.1:6379) unless `$cache` 
+                |   is passed as fourth argument.
+                | - If `isDevMode` is false, set then proxy classes have to be explicitly 
+                |   created through the command line.
+                |
+                */
                 'dev_mode' => true,
+
+                /*
+                |----------------------------------------------------------------------------
+                | Cache
+                |----------------------------------------------------------------------------
+                |
+                | Do not use Doctrine without a metadata and query cache! Doctrine is 
+                | optimized for working with caches. The recommended cache driver to use 
+                | with Doctrine is APC.
+                | 
+                | More Information, see Doctrine documentation:
+                | https://www.doctrine-project.org/projects/doctrine-orm/en/2.7/reference/advanced-configuration.html
+                |
+                */
                 'cache_dir' => null,
                 'metadata_dirs' => null,
+
+                /*
+                |----------------------------------------------------------------------------
+                | Proxy directory
+                |----------------------------------------------------------------------------
+                |
+                | If `proxyDir` is not set, use the systems temporary directory.
+                |
+                */
                 'proxy_dir' => null,
+
+                /*
+                |----------------------------------------------------------------------------
+                | Annotation Reader
+                |----------------------------------------------------------------------------
+                |
+                */
                 'useSimpleAnnotationReader' => false,
+
+                /*
+                |----------------------------------------------------------------------------
+                | Connection
+                |----------------------------------------------------------------------------
+                |
+                | More Information, see Doctrine documentation:
+                | https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html 
+                |
+                */
                 'connection' => [
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Driver
+                    |----------------------------------------------------------------------------
+                    |
+                    | A MySQL driver that uses the pdo_mysql PDO extension.
+                    |
+                    */
                     'driver' => 'pdo_mysql',
-                    'host' => 'localhost',
-                    'user' => 'root',
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Host
+                    |----------------------------------------------------------------------------
+                    |
+                    | Hostname of the database to connect to.
+                    |
+                    */
+                    'host' => '',
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | User
+                    |----------------------------------------------------------------------------
+                    |
+                    | Username to use when connecting to the database.
+                    |
+                    */
+                    'user' => '',
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Password
+                    |----------------------------------------------------------------------------
+                    |
+                    | Password to use when connecting to the database.
+                    |
+                    */
                     'password' => '',
-                    'dbname' => 'workflow',
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Database name
+                    |----------------------------------------------------------------------------
+                    |
+                    | Name of the database/schema to connect to.
+                    |
+                    */
+                    'dbname' => '',
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Port
+                    |----------------------------------------------------------------------------
+                    |
+                    | Port of the database to connect to.
+                    |
+                    */
+                    'port' => 3306,
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Unix socket
+                    |----------------------------------------------------------------------------
+                    |
+                    | Name of the socket used to connect to the database.
+                    |
+                    */
+                    'unix_socket' => '',
+
+                    /*
+                    |----------------------------------------------------------------------------
+                    | Charset
+                    |----------------------------------------------------------------------------
+                    |
+                    | The charset used when connecting to the database.
+                    |
+                    */
                     'charset' => 'utf8mb4'
                 ]
             ],
