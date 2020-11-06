9e03d4e3e88fcbc278b21083b3742885d0bbfb73
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-11-06 13:20:09 +0100

    #IMPLEMENT 'service for priority finder'

---
+++ src/Domain/Priority/Service/PriorityFinder.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Domain\Priority\Repository\PriorityFinderRepository;
+
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 Heinrich Schiller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace App\Domain\Priority\Service;
+
+use App\Domain\Priority\Repository\PriorityFinderRepository;
+
+final class PriorityFinder
+{
+    /**
+     * @Injection
+     * @var PriorityFinderRepository
+     */
+    private PriorityFinderRepository $repository;
+
+    /**
+     * The constructor
+     * 
+     * @param PriorityFinderRepository
+     */
+    public function __construct(PriorityFinderRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Find all priority values
+     * 
+     * @return array List of priority values
+     */
+    public function findAll(): array
+    {
+        return $this->repository->findAll();
+    }
+}
