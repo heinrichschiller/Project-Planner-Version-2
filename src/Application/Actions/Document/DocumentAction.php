3c38c4917b8709d7d60cc7977fa7995bd2a5ad2f
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-11-17 11:58:25 +0100

    #IMPLEMENTATION 'document action'

---
+++ src/Application/Actions/Document/DocumentAction.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 Heinrich Schiller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace App\Application\Actions\Document;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class DocumentAction
+{
+    private $ci;
+
+    public function __construct(ContainerInterface $ci)
+    {
+        $this->ci = $ci;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args = []): Response
+    {
+        $html = $this->ci->get('view')->render('document/index');
+        $response->getBody()->write($html);
+
+        return $response;
+    }
+}
