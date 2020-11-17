912951a2877c7604e1bb4e282cbaa386a14b8554
Heinrich Schiller <heinrich@heinrich-schiller.de>
2020-11-17 14:04:38 +0100

    #CHANGE 'code rewrite'

--- src/Application/Actions/Dashboard/DashboardAction.php
+++ src/Application/Actions/Dashboard/DashboardAction.php
@@ -2,14 +2,24 @@
 
 namespace App\Application\Actions\Dashboard;
 
-use App\Application\Actions\Action;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class DashboardAction extends Action
+class DashboardAction
 {
+    private $ci;
+
+    public function __construct(ContainerInterface $ci)
+    {
+        $this->ci = $ci;
+    }
+
     public function __invoke(Request $request, Response $response, $args = []): Response
     {
-        return $this->render($response, 'dashboard/index');
+        $html = $this->ci->get('view')->render('dashboard/index');
+        $response->getBody()->write($html);
+
+        return $response;
     }
 }
