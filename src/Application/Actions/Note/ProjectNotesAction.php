11bffbce8bb43b1ab40a79672f3040d79ffa8a1d
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-01-08 09:37:17 +0100

    #IMPLEMENT 'notes action'

---
+++ src/Application/Actions/Note/ProjectNotesAction.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 Heinrich Schiller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace App\Application\Actions\Note;
+
+use App\Domain\Note\Service\ProjectNotesFinder;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ProjectNotesAction
+{
+    /**
+     * @Injection
+     * @var ProjectNotesFinder
+     */
+    private ProjectNotesFinder $noteFinder;
+
+    /**
+     * The constructor
+     * 
+     * @param ProjectNotesFinder $noteFinder
+     */
+    public function __construct(ProjectNotesFinder $noteFinder)
+    {
+        $this->noteFinder = $noteFinder;
+    }
+
+    /**
+     * The invoker
+     * 
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * 
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, array $args = []): Response
+    {
+        $data = json_encode($this->noteFinder->findAll($args['id']));
+
+        $response->getBody()->write($data);
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    }
+}
