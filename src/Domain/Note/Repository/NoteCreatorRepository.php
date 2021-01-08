a9374cd8d92a775dec8077ca0dc9744d61be5ee1
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-01-08 09:39:13 +0100

    #IMPLEMENT 'note creator repository'

---
+++ src/Domain/Note/Repository/NoteCreatorRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 Heinrich Schiller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace App\Domain\Note\Repository;
+
+use DateTime;
+use Entities\Note;
+use Doctrine\ORM\EntityManager;
+
+class NoteCreatorRepository
+{
+    /**
+     * @Injection
+     * @var EntityManager
+     */
+    private EntityManager $entityManager;
+
+    /**
+     * The constructor
+     * 
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->entityManager = $em;
+    }
+
+    /**
+     * Insert note
+     * 
+     * @param array $formData The form data
+     */
+    public function insertNote(array $formData)
+    {
+        $note = new Note;
+
+        $note->setTitle($formData['title']);
+        $note->setDescription($formData['desc']);
+        $note->setProjectId($formData['projectId']);
+        $note->setCreatedAt(new DateTime('now'));
+
+        $this->entityManager->persist($note);
+        $this->entityManager->flush();
+    }
+}
