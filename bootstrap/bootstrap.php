c4b8ec938e1941a0e9cedfd642fa0e50788bb57f
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-02-02 23:41:45 +0100

    #CHANGE 'include helper function'

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -36,6 +36,7 @@ error_reporting(-1);
 ini_set('display_errors', '1');
 
 require __DIR__ . '/../app/constants.php';
+require __DIR__ . '/../app/helper.php';
 require ROOT_DIR . 'vendor/autoload.php';
 
 /*
