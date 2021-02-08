9c7e446eb2ce092e87d6a2a2bd40a7f23d4a9903
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-02-08 02:11:53 +0100

    code rewrite

--- src/Application/Actions/Email/EmailAction.php
+++ src/Application/Actions/Email/EmailAction.php
@@ -30,23 +30,22 @@ declare(strict_types = 1);
 
 namespace App\Application\Actions\Email;
 
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\Mustache;
 
 class EmailAction
 {
-    private $ci;
+    private $view;
 
-    public function __construct(ContainerInterface $ci)
+    public function __construct(Mustache $view)
     {
-        $this->ci = $ci;
+        $this->view = $view;
     }
 
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
-        $html = $this->ci->get('view')->render('email/index');
-        $response->getBody()->write($html);
+        $response = $this->view->render($response, 'email/index');
 
         return $response;
     }
