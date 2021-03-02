3a1ba02af9cc001a580831e56e0413180db5b076
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-03-02 01:54:14 +0100

    update

--- src/Application/Actions/Issue/IssueAction.php
+++ src/Application/Actions/Issue/IssueAction.php
@@ -30,23 +30,40 @@ declare(strict_types = 1);
 
 namespace App\Application\Actions\Issue;
 
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\Mustache;
 
 class IssueAction
 {
-    private $ci;
+    /**
+     * @Injection
+     * @var Mustache
+     */
+    private Mustache $view;
 
-    public function __construct(ContainerInterface $ci)
+    /**
+     * The constructor
+     * 
+     * @param Mustache $view
+     */
+    public function __construct(Mustache $view)
     {
-        $this->ci = $ci;
+        $this->view = $view;
     }
 
+    /**
+     * The invoker
+     * 
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * 
+     * @return Response
+     */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
-        $html = $this->ci->get('view')->render('issue/index');
-        $response->getBody()->write($html);
+        $this->view->render($response, 'issue/index');
 
         return $response;
     }
