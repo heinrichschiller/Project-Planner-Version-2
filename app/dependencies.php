a61b5fb0d5b1e16ae1611eaa747df8526630237c
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-04-15 14:35:57 +0200

    update

--- app/dependencies.php
+++ app/dependencies.php
@@ -59,6 +59,8 @@ use App\Domain\Contact\Repository\ContactUpdatingRepository;
 use App\Domain\Contact\Service\ContactCreator;
 use App\Domain\Contact\Service\ContactFinder;
 use App\Domain\Contact\Service\ContactReader;
+use App\Domain\Dashboard\Service\ImportantProjectFinder;
+use App\Domain\Dashboard\Service\ImportantTaskFinder;
 use App\Domain\Note\Repository\NoteCreatorRepository;
 use App\Domain\Priority\Repository\PriorityFinderRepository;
 use App\Domain\Priority\Service\PriorityFinder;
@@ -167,6 +169,8 @@ return function(ContainerBuilder $builder)
         DashboardAction::class => function(ContainerInterface $container): DashboardAction
         {
             return new DashboardAction(
+                $container->get(ImportantProjectFinder::class),
+                $container->get(ImportantTaskFinder::class),
                 $container->get(Mustache::class)
             );
         },
