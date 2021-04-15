dd0ae9a97983d966322631a7143b422d6b10a799
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-04-15 14:31:09 +0200

    added

---
+++ src/Domain/Dashboard/Repository/ImportantTaskFinderRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 Heinrich Schiller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+declare( strict_types = 1 );
+
+namespace App\Domain\Dashboard\Repository;
+
+use Doctrine\ORM\EntityManager;
+
+class ImportantTaskFinderRepository
+{
+    /**
+     * @Injection
+     * @var EntityManager
+     */
+    private EntityManager $entityManager;
+
+    /**
+     * The constructor
+     * 
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Find all important tasks
+     * 
+     * @return array List of tasks
+     */
+    public function findAll(): array
+    {
+        return (array) $this->entityManager
+            ->createQueryBuilder()
+            ->select('t')
+            ->from('Entities\Task', 't')
+            ->where('t.priorityId = 1 OR t.priorityId = 2')
+            ->andWhere('t.statusId != 5 AND t.statusId != 6')
+            ->orderBy('t.priorityId', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+}
