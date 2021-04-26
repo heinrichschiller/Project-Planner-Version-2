f2cede5f4ef11d38d36dc2ae01acd46a8f84636b
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-04-26 20:17:11 +0200

    added, tinymce for notes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,3 +5,15 @@ tinymce.init({
     language: 'en'
 });
 
+tinymce.init({
+    menubar: false,
+    statusbar: false,
+    toolbar: 'bold italic underline | link',
+    plugins: 'link',
+    selector: '#note__desc',
+    setup: function (editor) {
+        editor.on('change', function () {
+            tinymce.triggerSave();
+        });
+    }
+});
