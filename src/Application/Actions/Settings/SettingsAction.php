26fb41f0d5b78a7b04b432f2ee286c18e2850703
Heinrich Schiller <heinrich@heinrich-schiller.de>
2021-07-07 22:25:32 +0200

    removed mustache, return response as json

--- src/Application/Actions/Settings/SettingsAction.php
+++ src/Application/Actions/Settings/SettingsAction.php
@@ -32,24 +32,17 @@ namespace App\Application\Actions\Settings;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Views\Mustache;
 
 class SettingsAction
 {
-    /**
-     * @Injection
-     * @var Mustache
-     */
-    private Mustache $view;
-
     /**
      * The constructor
      * 
      * @param Mustache $view Mustache engine
      */
-    public function __construct(Mustache $view)
+    public function __construct()
     {
-        $this->view = $view;
+        // put your code here
     }
 
     /**
@@ -63,8 +56,17 @@ class SettingsAction
      */
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
-        $this->view->render($response, 'settings/index');
+        $data = [
+            'success' => [
+                'status' => 200,
+                'message' => 'Logic for settings is not implemented yet.'
+            ]
+        ];
 
-        return $response;
+        $response->getBody()->write(json_encode($data));
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
     }
 }
